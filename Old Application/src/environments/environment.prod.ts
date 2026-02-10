74a9b9ddad9d6402bec4841b87e4b5ec805fb70d
hpm <philipp.seydel@mbbm-vas.com>
2026-02-10 10:07:52 +0100

    Remove OldApplication

--- Old Application/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://pnnteiwvfyltusrrcmbf.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubnRlaXd2ZnlsdHVzcnJjbWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNDU4NDcsImV4cCI6MjA4MDkyMTg0N30.p0FqSUu_u4OBg5rfy5iJvvU77kWWLcs-3FgAbKNfGYI',
-};
--- Old Application/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://tbibfauxuqdhjyiptran.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiaWJmYXV4dXFkaGp5aXB0cmFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4NjA0MjUsImV4cCI6MjA4MjQzNjQyNX0.pzJN0XOEYVVmT4yidd1RAodIH4hNJ3m_2FaTXI7jpuU',
-};

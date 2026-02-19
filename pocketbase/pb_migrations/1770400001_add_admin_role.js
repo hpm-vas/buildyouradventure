759abdc94a9ee884f6d20c292d1fc31a537b0f9d
hpm <philipp.seydel@mbbm-vas.com>
2026-02-19 12:24:05 +0100

    Remove pocketbase folder

--- pocketbase/pb_migrations/1770400001_add_admin_role.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="../pb_data/types.d.ts" />
-migrate((app) => {
-  // No-op: admin role removed, only gamemaster is used
-  // This migration previously added 'admin' role but is now obsolete
-}, (app) => {
-  // No-op revert
-});
